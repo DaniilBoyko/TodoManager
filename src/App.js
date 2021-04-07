5b832af5cc76a3c27c66a7bd92a365a93e3d7b66
DaniilBoyko <daniil.boyko.2017@gmail.com>
2021-04-08 00:04:09 +0300

    Initial project commit
    Initial project commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./Todo/AddTodo";
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, completed: false, title: "By bred" },
+    { id: 2, completed: true, title: "By butter" },
+    { id: 3, completed: false, title: "By bred" },
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([
+        {
+          title,
+          id: Date.now(),
+          completed: false,
+        },
+      ])
+    );
+  }
+
+  return (
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo} />
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>No Todos!</p>
+        )}
+      </div>
+    </Context.Provider>
+  );
+}
+
+export default App;
